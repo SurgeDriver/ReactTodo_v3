0bbaedcb89e51ff15c9ea6faf93a2ade892ce00b
SD <fuckyou@realmail.com>
2025-04-10 15:46:22 +0300

    clean up and icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,9 @@
-import { useState } from 'react'
-import './App.css'
-import YouTube from 'react-youtube';
-import './AudioPlayer.css'; // Добавьте эту строку
-import AudioPlayer from './AudioPlayer';
+// import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <h1>Blank</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Fuck you.
-        </p>
-      </div>
-      <AudioPlayer/>
     </>
   )
 }
